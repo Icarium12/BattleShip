f8aa4eec26c4279161e09eaea26238f91f91af6b
Praise Mogaji <praisemogaji@gmail.com>
2026-05-21 02:54:14 +0100

    started ship module/class

---
+++ src/ship.js
@@ -0,0 +1,18 @@
+class Ship {
+
+    constructor(length) {
+        this.length = length;
+        this.hitNum = 0;
+        this.isSunk = false;
+    }
+
+    hit() {
+        this.hitNum += 1;
+    }
+
+    isSunk() {
+        if (this.hitNum === this.length) {
+            this.isSunk = true;
+        }
+    }
+}
